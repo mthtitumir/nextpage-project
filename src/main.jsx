0f3cf241503c67b6d09d3ff51c44922189f4e17b
mthtitumir <titumirgccc@gmail.com>
2023-04-05 11:27:39 +0600

    books section complete

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Books from './components/Books';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'books',
+        element: <Books />,
+        loader: ()=>fetch('https://api.itbook.store/1.0/new'),
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
